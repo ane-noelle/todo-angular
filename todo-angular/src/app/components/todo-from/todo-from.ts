1f157b28654597118aaf3212f590ff7fd1d7fa5b
ane-noelle <anenoelle60@gmail.com>
2026-08-06 10:59:06 -0300

    dia 2

---
+++ todo-angular/src/app/components/todo-from/todo-from.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-from',
+  imports: [],
+  templateUrl: './todo-from.html',
+  styleUrl: './todo-from.css',
+})
+export class TodoFrom {}
